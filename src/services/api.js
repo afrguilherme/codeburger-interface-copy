9efa87162715a1c62e528194f281016dfa532daa
afrguilherme <augustogafr@gmail.com>
2024-04-05 15:03:54 -0300

    connecting to api on railway

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://codeburger-api-copy-production.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
